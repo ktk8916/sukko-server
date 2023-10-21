1050d86787988948b32d614bb3f12f167cf364f5
ktk8916 <ktk8916@naver.com>
2023-10-21 21:02:09 +0900

    init

---
+++ src/main/java/me/sukko/SukkoApplication.java
@@ -0,0 +1,13 @@
+package me.sukko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SukkoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SukkoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/sukko/sukko/SukkoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sukko.sukko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SukkoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
