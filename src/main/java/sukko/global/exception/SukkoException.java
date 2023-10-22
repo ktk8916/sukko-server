81344cb8390c9599509ccdcdbafabd4cbe55e083
ktk8916 <ktk8916@naver.com>
2023-10-22 18:45:55 +0900

    getter 추가

--- src/main/java/sukko/global/exception/SukkoException.java
+++ src/main/java/sukko/global/exception/SukkoException.java
@@ -1,11 +1,13 @@
 package sukko.global.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public class SukkoException extends RuntimeException{
 
-    private String message;
     private HttpStatus httpStatus;
+    private String message;
 
     public SukkoException() {
         CustomExceptionType customExceptionType = CustomExceptionType.of(this.getClass());
