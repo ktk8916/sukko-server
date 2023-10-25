c905ef38290f740afdc17432c97d6d7c98a06561
ktk8916 <ktk8916@naver.com>
2023-10-26 01:38:31 +0900

    유효하지 않은 비밀번호 예외 추가

--- src/main/java/sukko/global/exception/CustomExceptionType.java
+++ src/main/java/sukko/global/exception/CustomExceptionType.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 import sukko.expedition.exception.InvalidCharacterNameException;
 import sukko.expedition.exception.NoMatchingClassTypeException;
 import sukko.expedition.exception.NoMatchingServerTypeException;
-import sukko.taxidermy.exception.NoMatchingDungeonType;
+import sukko.taxidermy.exception.InvalidTaxidermyPasswordException;
+import sukko.taxidermy.exception.NoMatchingDungeonTypeException;
 import sukko.taxidermy.exception.TaxidermyNotFoundException;
 
 import java.util.Arrays;
@@ -18,11 +19,12 @@ public enum CustomExceptionType {
     UNKNOWN(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 서버 에러", SukkoException.class),
     INVALID_CHARACTER_NAME(HttpStatus.BAD_REQUEST, "해당하는 캐릭터가 없습니다.", InvalidCharacterNameException.class),
 
-    NO_MATCHING_DUNGEON_TYPE(HttpStatus.BAD_REQUEST, "일치하는 레이드가 없습니다.", NoMatchingDungeonType.class),
+    NO_MATCHING_DUNGEON_TYPE(HttpStatus.BAD_REQUEST, "일치하는 레이드가 없습니다.", NoMatchingDungeonTypeException.class),
     NO_MATCHING_CLASS_TYPE(HttpStatus.BAD_REQUEST, "일치하는 직업이 없습니다.", NoMatchingClassTypeException.class),
     NO_MATCHING_SERVER_TYPE(HttpStatus.BAD_REQUEST, "일치하는 서버가 없습니다.", NoMatchingServerTypeException.class),
 
     TAXIDERMY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 글이 없습니다.", TaxidermyNotFoundException.class),
+    INVALID_TAXIDERMY_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.", InvalidTaxidermyPasswordException.class),
 
     ;
 
