bdc9c846398257eeae57b80d0e774b4e7053d838
ktk8916 <ktk8916@naver.com>
2023-10-29 14:47:02 +0900

    cors

--- src/main/java/sukko/config/WebConfig.java
+++ src/main/java/sukko/config/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET","POST","PUT","PATCH","DELETE")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("https://sukko.me");
     }
 }
